204cb4e6126733365edd11f7a7c874e9d65c0cc8
David Giraldo <davidgiraldobueno@gmail.com>
2020-08-19 22:19:43 -0500

    register

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors());
 
 const database = {
   users: [
@@ -35,7 +38,7 @@ app.post("/signin", (req, res) => {
     req.body.email === database.users[0].email &&
     req.body.password === database.users[0].password
   ) {
-    res.json("success");
+    res.json(database.users[0]);
   } else {
     res.status(400).json("error login in");
   }
@@ -43,12 +46,16 @@ app.post("/signin", (req, res) => {
 
 app.post("/register", (req, res) => {
   const { name, email, password } = req.body;
+
+  bcrypt.hash(password, 8, (err, hash) => {
+    console.log(hash);
+  });
+
   if (name && email && password) {
     database.users.push({
       id: "1235",
       name: name,
       email: email,
-      password: password,
       entries: 0,
       joined: new Date(),
     });
@@ -58,6 +65,35 @@ app.post("/register", (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server running in port 3000");
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+  if (!found) {
+    res.status(400).json("no such user");
+  }
+});
+
+app.put("/image", (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+  if (!found) {
+    res.status(400).json("not found");
+  }
+});
+
+app.listen(3001, () => {
+  console.log("Server running in port 3001");
 });
