a7c941a4727ef8c3fd2792115404ae4027937384
David Giraldo <davidgiraldobueno@gmail.com>
2020-08-22 15:19:59 -0500

    adding port

--- server.js
+++ server.js
@@ -69,6 +69,6 @@ app.post("/imageurl", (req, res) => {
   image.handleClarifaiApiCall(req, res);
 });
 
-app.listen(3001, () => {
-  console.log("Server running in port 3001");
+app.listen(process.env.PORT || 3001, () => {
+  console.log(`Server running in port ${process.env.PORT}`);
 });
